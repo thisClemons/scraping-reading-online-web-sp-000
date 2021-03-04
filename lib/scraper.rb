9aa88ced988280c23d0f045a6684f8a444a25340
Adam Clemons <adamlouisclemons@gmail.com>
2021-03-04 23:35:09 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,19 @@
-require 'nokogiri'
-require 'open-uri'
+require_relative '../config/environment'
+# require 'nokogiri'
+# require 'open-uri'
+# require 'pry'
 
-html = open("https://flatironschool.com/")
+html = open("https://flatironschool.com/our-courses/")
+
+doc = Nokogiri::HTML(html)
+
+# puts doc
+
+courses = doc.css(".title-oE5vT4")
+
+courses.each {|course| puts course.text.strip}
+
+p courses[0]
+
+
+binding.pry
